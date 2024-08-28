515c292309cece111b4897dd72266cfe6fd246f2
unknown <mukodazayofe@gmail.com>
2024-08-28 19:36:20 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,78 +1,109 @@
 /* eslint-disable no-undef */
+require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const Person = require('./models/person')
 
+app.use(express.static('dist'))
 app.use(express.json())
+// app.use(requestLogger)
 app.use(cors())
+mongoose.set('strictQuery', false)
 
-const persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// const persons = [
+//   {
+//     "id": "1",
+//     "name": "Arto Hellas",
+//     "number": "040-123456"
+//   },
+//   {
+//     "id": "2",
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523"
+//   },
+//   {
+//     "id": "3",
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345"
+//   },
+//   {
+//     "id": "4",
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122"
+//   }
+// ]
 
-app.get('/api/persons',(req,res) =>{
+
+const errorHandler = (error, request, response, next) => {
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler);
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
     res.json(persons)
+  })
 });
 
-app.get('/info',(req,res)=>{
-    const entryTime = new Date().toLocaleString();
-    const entryNum = persons.length;
+app.get('/info', (req, res) => {
+  const entryTime = new Date().toLocaleString();
+  const entryNum = persons.length;
 
-    res.send(`
+  res.send(`
         <p>Phonebook has info for ${entryNum} of people</p><br/>
         <p>${entryTime}</p>
         `)
 })
 
-app.get('/api/persons/:id',(req,res)=>{
-    const id = req.params.id
-    const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id).then(person => {
     res.json(person)
+  })
 })
 
-app.delete('/api/persons/:id',(req,res)=>{
-    const id = req.params.id
-    const persons = persons.filter(person => person.id !== id)
-    res.status(204).end
+app.put('api/persons/:id',(req,res,next)=>{
+  const {name, number} = body.req
+
+
+  Person.findByIdAndUpdate(req.params.id,{name,number},{new:true,runValidators:true, context:'query'})
+  .then(updatePerson=>{
+    res.json(updatePerson)
+  })
+  .catch(err=> next(err))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end
+    })
+    .catch(err => next(err))
 })
 
 
-app.post('/api/persons',(req,res)=>{
-    const person = req.body
+app.post('/api/persons', (req, res,next) => {
+  const person = req.body
 
-    if(!person.name || !person.number || person.name === newEntry.name){
-        return res.status(400).json({
-             error: 'name must be unique' 
-        })
-    }
-
-    const newEntry = {
-        name:person.name,
-        number:person.number,
-        id:Math.floor(Math.random()*10000)
-    }
-
-    const persons = persons.concat(newEntry)
-    res.json(newEntry)
+  const newEntry = new Person({
+    name: person.name,
+    number: person.number,
+    id: Math.floor(Math.random() * 10000)
+  }
+  )
+  newEntry.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+  .catch(err => next(err))
 
 })
 
